b55933d1a4c41799d86ef93d8f38df51ed34d945
Rodrigo Echaide <rodrigoechaide@gmail.com>
2021-03-30 16:27:50 +0200

    Added Python Flask examples

---
+++ python/flask/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+# app.debug = True
+
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+    return 'Blog Number %d' % postID
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+    return 'Revision Number %f' % revNo
+
+if __name__ == '__main__':
+   # app.run(host, port, debug, options)
+   app.run(debug = True)
