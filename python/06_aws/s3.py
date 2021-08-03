8b05041f768abd0585b73110a1c31406d3c628f9
Rodrigo Echaide <rechaide@pmintl.net>
2021-08-03 09:24:47 +0200

    Add boto3 s3 example script

---
+++ python/06_aws/s3.py
@@ -0,0 +1,111 @@
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/examples.html
+
+import boto3
+from botocore.exceptions import ClientError
+import logging
+import os
+
+def authenticate(aws_access_key_id, aws_secret_access_key, region_name):
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
+    return session
+
+def list_buckets(session):
+    # Let's use Amazon S3 resource method
+    s3 = session.resource('s3')
+
+    # Print out bucket names
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
+def upload_file(session, file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3 = session.client('s3')
+    try:
+        response = s3.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+def upload_fileobj(session, file_name, bucket, object_name=None, ExtraArgs=None):
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+    s3 = session.client('s3')
+    with open(file_name, "rb") as f:
+        if ExtraArgs is None:
+            s3.upload_fileobj(f, bucket, object_name)
+        else:
+            s3.upload_fileobj(f, bucket, object_name, ExtraArgs)
+    return True
+
+def delete_object(method, session, object_name, bucket):
+    if method == "resource":
+        s3 = session.resource('s3')
+        s3.Object(bucket, object_name).delete()
+    if method == "client":
+        s3 = session.client('s3')
+        try:
+            response = s3.delete_object(Key=object_name, Bucket=bucket)
+        except ClientError as e:
+            logging.error(e)
+            return False
+    return True
+
+def download_object(method, session, bucket, path, key):
+    if method == "resource":
+        s3 = session.resource("s3")
+        s3.meta.client.download_file(bucket, path + '/' + key, './' + key)
+    if method == "client":
+        s3 = session.client("s3")
+        s3.download_file(bucket, path + '/' + key, key)
+    return True
+
+def delete_folder(session, bucket, prefix):
+    """Delete all objects with the same path prefix
+
+    :param session: Session object
+    :param bucket: Bucket name
+    :param prefix: Prefix to delete objects under
+    """
+    s3 = session.resource("s3")
+    bucket = s3.Bucket(bucket)
+    bucket.objects.filter(Prefix=prefix).delete()
+
+def list_files():
+    pass
+
+
+### Main Program ###
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.environ.get('AWS_REGION')
+
+session = authenticate(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION)
+
+# response = upload_file(session, "dummy.txt", "qa-tradersync", "dummy/dummy.txt")
+# response = upload_fileobj(session, "dummy2.txt", "qa-tradersync", "dummy/dummy2.txt")
+# response = upload_fileobj(session, "picture.png", "qa-tradersync", "picture.png", {"ACL": "public-read", "ContentType": "image/png"})
+# response = delete_object("client", session, "dummy.txt", "qa-tradersync")
+# response = delete_object("resource", session, "picture.png", "qa-tradersync")
+# response = download_object("client", session, "qa-tradersync", "user_backups", "backup_adminuser_2021-08-02_17_31_23_1.csv")
+# response = delete_folder(session, "qa-tradersync", "dummy")
+
